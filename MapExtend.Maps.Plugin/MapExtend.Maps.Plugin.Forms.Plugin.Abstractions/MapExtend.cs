f955db741491a4aee9686af8668e396f76fbce8f
rafaelrmou <rafaelrmou2@hotmail.com>
2015-08-28 08:14:08 -0300

    Update MapExtend.cs
    Update MapExtendo to Work with Mode of Route
    
    RAIL	Rail.
    METRO_RAIL	Light rail transit.
    SUBWAY	Underground light rail.
    TRAM	Above ground light rail.
    MONORAIL	Monorail.
    HEAVY_RAIL	Heavy rail.
    COMMUTER_TRAIN	Commuter rail.
    HIGH_SPEED_TRAIN	High speed train.
    BUS	Bus.
    INTERCITY_BUS	Intercity bus.
    TROLLEYBUS	Trolleybus.
    SHARE_TAXI	Share taxi is a kind of bus with the ability to drop off and pick up passengers anywhere on its route.
    FERRY	Ferry.
    CABLE_CAR	A vehicle that operates on a cable, usually on the ground. Aerial cable cars may be of the type GONDOLA_LIFT.
    GONDOLA_LIFT	An aerial cable car.
    FUNICULAR	A vehicle that is pulled up a steep incline by a cable. A Funicular typically consists of two cars, with each car acting as a counterweight for the other.
    OTHER

--- MapExtend.Maps.Plugin/MapExtend.Maps.Plugin.Forms.Plugin.Abstractions/MapExtend.cs
+++ MapExtend.Maps.Plugin/MapExtend.Maps.Plugin.Forms.Plugin.Abstractions/MapExtend.cs
@@ -67,6 +67,35 @@ namespace Xam.Plugin.MapExtend.Abstractions
 
             return waypoints;
         }
+        
+        ///<sumary>
+        ///Get Directions with Mode
+        ///</sumary>
+        ///<param name="Mode">
+        //RAIL	Rail.
+        //METRO_RAIL	Light rail transit.
+        //SUBWAY	Underground light rail.
+        //TRAM	Above ground light rail.
+        //MONORAIL	Monorail.
+        //HEAVY_RAIL	Heavy rail.
+        //COMMUTER_TRAIN	Commuter rail.
+        //HIGH_SPEED_TRAIN	High speed train.
+        //BUS	Bus.
+        //INTERCITY_BUS	Intercity bus.
+        //TROLLEYBUS	Trolleybus.
+        //SHARE_TAXI	Share taxi is a kind of bus with the ability to drop off and pick up passengers anywhere on its route.
+        //FERRY	Ferry.
+        //CABLE_CAR	A vehicle that operates on a cable, usually on the ground. Aerial cable cars may be of the type GONDOLA_LIFT.
+        //GONDOLA_LIFT	An aerial cable car.
+        //FUNICULAR	A vehicle that is pulled up a steep incline by a cable. A Funicular typically consists of two cars, with each car acting as a counterweight for the other.
+        //OTHER
+        //</param>
+        private string getMapsApiDirectionsUrl(Position From, Position To, string Mode)
+        {
+            String waypoints = string.Format("http://216.58.222.10/maps/api/directions/json?origin={0},{1}&destination={2},{3}&sensor=false&mode={4}", From.Latitude.ToString().Replace(',', '.'), From.Longitude.ToString().Replace(',', '.'), To.Latitude.ToString().Replace(',', '.'), To.Longitude.ToString().Replace(',', '.'),Mode);
+
+            return waypoints;
+        }
 
         private async Task<Models.Places.Rootobject> DownloadPlaces(string url)
         {
